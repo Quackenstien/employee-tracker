4868c82341eda874132c5df2a949f6e0cfa42e6b
Quackenstien <donaldhawkins422@gmail.com>
2020-06-10 14:23:27 -0700

    building out the prompts to display in the terminal

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+//Creating connection
 var connection = mysql.createConnection({
   host: "localhost",
   // Your port; if not 3306
@@ -12,3 +13,30 @@ var connection = mysql.createConnection({
   password: "",
   database: "employee_trackerDB",
 });
+
+//Calling the prompts after the connection is ran
+connection.connect(function (err) {
+  if (err) throw err;
+  runTracker();
+});
+
+//Displaying the prompts to the user in the terminal
+function runTracker() {
+  inquirer.prompt([
+    {
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Add a department",
+        "Add a role",
+        "add an employee",
+        "View a department",
+        "View a role",
+        "View an employee",
+        "Update employee role",
+        "Exit",
+      ],
+    },
+  ]);
+}
