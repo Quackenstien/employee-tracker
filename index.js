9ab500c46450ea8887065d9f84db54c9471639dd
Quackenstien <donaldhawkins422@gmail.com>
2020-06-10 14:20:03 -0700

    creating connection

---
+++ index.js
@@ -0,0 +1,14 @@
+//Dependencies
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  // Your port; if not 3306
+  port: 3306,
+  // Your username
+  user: "root",
+  // Your password
+  password: "",
+  database: "employee_trackerDB",
+});
